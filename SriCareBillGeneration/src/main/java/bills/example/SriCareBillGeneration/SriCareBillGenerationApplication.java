7fbec6761b132dbf1d9a580c7403305ce889f3a5
Tharindi Senadeera <tia.tharindi@gmail.com>
2023-09-19 14:06:32 +0530

    1st commit

---
+++ SriCareBillGeneration/src/main/java/bills/example/SriCareBillGeneration/SriCareBillGenerationApplication.java
@@ -0,0 +1,13 @@
+package bills.example.SriCareBillGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriCareBillGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SriCareBillGenerationApplication.class, args);
+	}
+
+}
---
+++ SriCareBillGeneration/src/test/java/bills/example/SriCareBillGeneration/SriCareBillGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package bills.example.SriCareBillGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriCareBillGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
